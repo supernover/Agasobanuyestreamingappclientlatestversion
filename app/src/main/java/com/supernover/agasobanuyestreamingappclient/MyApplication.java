eeed3e1754bee32ec8312bd18fb79bbf798fd9d9
Hp <iradukundadidier2@gmail.com>
2021-01-06 22:11:55 +0200

    ads open

---
+++ app/src/main/java/com/supernover/agasobanuyestreamingappclient/MyApplication.java
@@ -0,0 +1,26 @@
+package com.supernover.agasobanuyestreamingappclient;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.appopen.AppOpenAd;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private AppOpenAdManager appOpenAdManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MobileAds.initialize(this);
+
+        appOpenAdManager = new AppOpenAdManager
+                .Builder(this, AppOpenAdManager.TEST_AD_UNIT_ID)
+                .build();
+    }
+
+    public AppOpenAdManager getAppOpenAdManager() {
+        return this.appOpenAdManager;
+    }
+}
