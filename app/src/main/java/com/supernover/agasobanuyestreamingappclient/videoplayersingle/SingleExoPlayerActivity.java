a7facda160c844175f748528abd3f62dba048774
Hp <iradukundadidier2@gmail.com>
2021-01-11 10:07:40 +0200

    ads open

--- app/src/main/java/com/supernover/agasobanuyestreamingappclient/videoplayersingle/SingleExoPlayerActivity.java
+++ app/src/main/java/com/supernover/agasobanuyestreamingappclient/videoplayersingle/SingleExoPlayerActivity.java
@@ -1,14 +1,13 @@
 package com.supernover.agasobanuyestreamingappclient.videoplayersingle;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
@@ -17,28 +16,14 @@ import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.rewarded.RewardItem;
-import com.google.android.gms.ads.rewarded.RewardedAd;
-import com.google.android.gms.ads.rewarded.RewardedAdCallback;
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.supernover.agasobanuyestreamingappclient.R;
-import com.supernover.agasobanuyestreamingappclient.details.FeatureDetailsActivity;
 
 public class SingleExoPlayerActivity extends AppCompatActivity {
     private String VIDEO_TITLE;
     private String VIDEO_URL;
     private PlayerView playerView;
-    private SimpleExoPlayer simpleExoPlayer;
-    private RewardedAd rewardedAd;
-
+    ExoPlayer exoPlayer;
 
     /* access modifiers changed from: protected */
     @Override // androidx.activity.ComponentActivity, androidx.core.app.ComponentActivity, androidx.appcompat.app.AppCompatActivity, androidx.fragment.app.FragmentActivity
@@ -51,101 +36,25 @@ public class SingleExoPlayerActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(this.VIDEO_TITLE);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-
-        rewardedAd = new RewardedAd(this,
-                "ca-app-pub-3063877521249388/6925345359");
-        RewardedAdLoadCallback adLoadCallback = new RewardedAdLoadCallback() {
-            @Override
-            public void onRewardedAdLoaded() {
-                // Ad successfully loaded.
-            }
-
-            @Override
-            public void onRewardedAdFailedToLoad(LoadAdError adError) {
-                // Ad failed to load.
-            }
-        };
-        rewardedAd.loadAd(new AdRequest.Builder().build(), adLoadCallback);
     }
 
     /* access modifiers changed from: protected */
     @Override // androidx.appcompat.app.AppCompatActivity, androidx.fragment.app.FragmentActivity
     public void onStart() {
-        if (rewardedAd.isLoaded()) {
-            Activity activityContext = SingleExoPlayerActivity.this;
-            RewardedAdCallback adCallback = new RewardedAdCallback() {
-                @Override
-                public void onRewardedAdOpened() {
-                    // Ad opened.
-                }
-
-                @Override
-                public void onRewardedAdClosed() {
-                    // Ad closed.
-                }
-
-                @Override
-                public void onUserEarnedReward(@NonNull RewardItem reward) {
-                    // User earned reward.
-                }
-
-                @Override
-                public void onRewardedAdFailedToShow(AdError adError) {
-                    // Ad failed to display.
-                }
-            };
-            rewardedAd.show(activityContext, adCallback);
-        } else {
-            Log.d("TAG", "The rewarded ad wasn't loaded yet.");
-        }
         super.onStart();
         this.playerView = (PlayerView) findViewById(R.id.exo_player_view);
         SimpleExoPlayer newSimpleInstance = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter.Builder(getApplicationContext()).build())));
-        this.simpleExoPlayer = newSimpleInstance;
+        this.exoPlayer = newSimpleInstance;
         this.playerView.setPlayer(newSimpleInstance);
-        this.simpleExoPlayer.prepare(new ExtractorMediaSource.Factory(new DefaultDataSourceFactory(this, Util.getUserAgent(this, "appname"))).createMediaSource(Uri.parse(this.VIDEO_URL)));
-        this.simpleExoPlayer.setPlayWhenReady(true);
+        this.exoPlayer.prepare(new ExtractorMediaSource.Factory(new DefaultDataSourceFactory(this, Util.getUserAgent(this, "appname"))).createMediaSource(Uri.parse(this.VIDEO_URL)));
+        this.exoPlayer.setPlayWhenReady(true);
     }
 
     /* access modifiers changed from: protected */
     @Override // androidx.appcompat.app.AppCompatActivity, androidx.fragment.app.FragmentActivity
     public void onDestroy() {
-
-        if (rewardedAd.isLoaded()) {
-            Activity activityContext = SingleExoPlayerActivity.this;
-            RewardedAdCallback adCallback = new RewardedAdCallback() {
-                @Override
-                public void onRewardedAdOpened() {
-                    // Ad opened.
-                }
-
-                @Override
-                public void onRewardedAdClosed() {
-                    // Ad closed.
-                }
-
-                @Override
-                public void onUserEarnedReward(@NonNull RewardItem reward) {
-                    // User earned reward.
-                }
-
-                @Override
-                public void onRewardedAdFailedToShow(AdError adError) {
-                    // Ad failed to display.
-                }
-            };
-            rewardedAd.show(activityContext, adCallback);
-        } else {
-            Log.d("TAG", "The rewarded ad wasn't loaded yet.");
-        }
         super.onDestroy();
-        this.simpleExoPlayer.release();
+        this.exoPlayer.release();
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
